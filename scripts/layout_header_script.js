3617eab7c87834aa508c0e9bea079a8bbbe6b4f4
caiguanhao <caiguanhao@gmail.com>
2013-05-16 00:40:09 +0800

    complete product details page;

--- scripts/layout_header_script.js
+++ scripts/layout_header_script.js
@@ -21,6 +21,33 @@ $(function(){
 		$("img.lazy").lazyload({ threshold : 400 });
 	}
 	$().UItoTop({ easingType: 'easeOutQuart' });
+    if ($("#etalage").length == 1) {
+		$("#etalage").etalage({
+			thumb_image_width: 250,
+			thumb_image_height: 250,
+			source_image_width: 600,
+			source_image_height: 600,
+			zoom_area_width: 300,
+			zoom_area_height: 300,
+			zoom_area_distance: 5,
+			small_thumbs: 4,
+			smallthumb_inactive_opacity: 0.3,
+			smallthumbs_position: "bottom",
+			speed: 200,
+			show_icon: false,
+			autoplay: false,
+			keyboard: false,
+			zoom_easing: false
+		});
+		$('#etalage').bind('contextmenu', function(){return false;});
+	}
+	$(".tabbtn").bind("click", function () {
+		var a = $(this).parent().index();
+		$(".tabbtn").removeClass("sel");
+		$(this).addClass("sel");
+		$(".maincontent div.mc").hide();
+		$(".maincontent div").eq(a).show()
+	});
 });
 /* totop */
 jQuery.extend(jQuery.easing,{easeOutQuart:function(e,a,b,c,d){return-c*((a=a/d-1)*a*a*a-1)+b}});
